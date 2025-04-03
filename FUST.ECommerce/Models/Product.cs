131babf67b16981cf4361132cdb0fa8b54f3e261
Davide Frate <davide.frate.2001@gmail.com>
2025-04-03 22:20:45 +0200

    sistemazione

--- FUST.ECommerce/Models/Product.cs
+++ FUST.ECommerce/Models/Product.cs
@@ -8,6 +8,6 @@
         public int Quantity { get; set; }
         public decimal Price { get; set; }
 
-        public Category Category { get; set; }
+        //public Category Category { get; set; }
     }
 }
